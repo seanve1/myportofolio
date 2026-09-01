87d5e69817912c35bcc91f49d4cd8cfde73b5ec3
Jotham Seanvedi Takin Allo <jotham.seanvedi@ui.ac.id>
2026-09-02 01:23:47 +0700

    Configure production settings and WhiteNoise

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-5%c_o9s5d_zrs-@rn2px^v(_g^gp=346q+q1wj8f0=+l&v3ad2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "jotham-seanvedi.pws.cs.ui.ac.id"]
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -135,6 +136,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+WHITENOISE_USE_FINDERS = True
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
