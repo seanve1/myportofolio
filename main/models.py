4903d414fe7da9abc208558902ca05d6d8eda6ae
Jotham Seanvedi Takin Allo <jotham.seanvedi@ui.ac.id>
2026-09-14 02:09:39 +0700

    feat: add logo field to Education model, update template, and implement static card styling

--- main/models.py
+++ main/models.py
@@ -25,6 +25,7 @@ class Education(models.Model):
     degree = models.CharField(max_length=255)
     start_year = models.CharField(max_length=4)
     end_year = models.CharField(max_length=4, blank=True, null=True)
+    logo = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
         return self.school_name
